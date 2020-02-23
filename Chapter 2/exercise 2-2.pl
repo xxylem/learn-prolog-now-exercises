5624712213076d6e48b3518bc23f1641a3d40245
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-02-23 12:07:32 +0000

    Add chapter 2 exercises.

---
+++ Chapter 2/exercise 2-2.pl
@@ -0,0 +1,10 @@
+house_elf(dobby).
+witch(hermione).
+witch('McGonagall').
+witch(rita_skeeter).
+magic(X) :- house_elf(X).
+magic(X) :- wizard(X).
+magic(X) :- witch(X).
+
+% To stop error
+wizard(bob).
