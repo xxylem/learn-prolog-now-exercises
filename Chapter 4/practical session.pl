3267b7d8020b35d435bf47a3bc48ca7384a57f24
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-03-08 19:52:05 +0000

    Finish chapter 4

--- Chapter 4/practical session.pl
+++ Chapter 4/practical session.pl
@@ -1,2 +1,8 @@
 combine1([], [], []).
 combine1([X|Xt], [Y|Yt], [X, Y | XYt]) :- combine1(Xt, Yt, XYt).
+
+combine2([], [], []).
+combine2([X|Xt], [Y|Yt], [[X, Y] | XYt]) :- combine2(Xt, Yt, XYt).
+
+combine3([], [], []).
+combine3([X|Xt], [Y|Yt], [j(X, Y) | XYt]) :- combine3(Xt, Yt, XYt).
