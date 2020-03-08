d773310657f456d4a6dc594fbab650d609297d9c
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-03-08 18:41:22 +0000

    Extend the predicate travel/3. It not only tells you the route to take to get from one place to another, but also how you have to travel.

--- Chapter 3/travel.pl
+++ Chapter 3/travel.pl
@@ -27,13 +27,13 @@ travel(X, Y) :- byPlane(X, Z),
                     travel(Z, Y).
 
 
-travel(X, Y, go(X, Y)) :- byCar(X, Y).
-travel(X, Y, go(X, Y)) :- byTrain(X, Y).
-travel(X, Y, go(X, Y)) :- byPlane(X, Y).
+travel(X, Y, car(X, Y)) :- byCar(X, Y).
+travel(X, Y, train(X, Y)) :- byTrain(X, Y).
+travel(X, Y, plane(X, Y)) :- byPlane(X, Y).
 
-travel(X, Y, go(X, Z, W)) :- byCar(X, Z),
+travel(X, Y, car(X, Z, W)) :- byCar(X, Z),
                                 travel(Z, Y, W).
-travel(X, Y, go(X, Z, W)) :- byTrain(X, Z),
+travel(X, Y, train(X, Z, W)) :- byTrain(X, Z),
                                 travel(Z, Y, W).
-travel(X, Y, go(X, Z, W)) :- byPlane(X, Z),
+travel(X, Y, plane(X, Z, W)) :- byPlane(X, Z),
                                 travel(Z, Y, W).
